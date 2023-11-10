f04a01657f040546c558913d65f7ec4153a6ae58
jkatsanis <katsanis18@gmail.com>
2023-11-10 19:42:59 +0100

    starting to really grind lol

--- assets/Scripts/game.cpp
+++ assets/Scripts/game.cpp
@@ -4,12 +4,15 @@ void Game::start()
 {
 	this->m_controller.start(*this);
 	this->m_controller_cam.start(this->m_controller.getPlayer());
+	//this->m_enemy_behaviour.start(this->config);
 }
 
 void Game::update()
 {	
 	std::string fps = std::to_string(s2d::Time::fps);
-	ImGui::MenuItem(fps.c_str());
+	ImGui::Text(fps.c_str());
+
 	this->m_controller.update();
 	this->m_controller_cam.update();
+	//this->m_enemy_behaviour.update();
 }
--- assets/Scripts/game.h
+++ assets/Scripts/game.h
@@ -4,12 +4,15 @@
 
 #include <Player/playerController.h>
 #include <Camera/cameraController.h>
+#include <Enemy/enemyBehaviour.h>
 
 class Game : public s2d::Base
 {
 private:
 	PlayerController m_controller;
 	CameraController m_controller_cam;
+	EnemyBehaviour m_enemy_behaviour;
+	
 public:
 	s2d::EngineConfig config;
 
