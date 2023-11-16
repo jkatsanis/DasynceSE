63b40e55b2f3f91125dda2dac0a046b82f448446
jkatsanis <katsanis18@gmail.com>
2023-11-16 13:26:50 +0100

    updatet light shader

--- assets/Sprites/Light/lightController.cpp
+++ assets/Sprites/Light/lightController.cpp
@@ -2,7 +2,7 @@
 
 void LightController::start(s2d::EngineConfig& config)
 {
-	this->m_lamp_light  = 
+	// this->m_lamp_light = config.ptr_sprites->getSpriteWithName("");
 }
 
 void LightController::update()
