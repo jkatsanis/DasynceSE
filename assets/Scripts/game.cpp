02f5d006f0df97c30eee42c8e6b4dccc23716bb9
jkatsanis <katsanis18@gmail.com>
2023-11-30 20:14:20 +0100

    progresss

--- assets/Scripts/game.cpp
+++ assets/Scripts/game.cpp
@@ -1,18 +1,11 @@
-#include "game.h"
+#include "Game.h"
 
-void Game::start()
+void Game::Start()
 {
-	this->m_controller.start(*this);
-	this->m_controller_cam.start(this->m_controller.getPlayer());
-	//this->m_enemy_behaviour.start(this->config);
+
 }
 
-void Game::update()
+void Game::Update()
 {	
-	std::string fps = std::to_string(s2d::Time::fps);
-	ImGui::Text(fps.c_str());
 
-	this->m_controller.update();
-	this->m_controller_cam.update();
-	//this->m_enemy_behaviour.update();
 }
--- assets/Scripts/game.h
+++ assets/Scripts/game.h
@@ -1,22 +1,15 @@
 #pragma once
 
-#include <_header/SpriteEngine.h>
+#include "IScript.h"
 
-#include <Player/playerController.h>
-#include <Camera/cameraController.h>
-#include <Enemy/enemyBehaviour.h>
-
-class Game : public s2d::Base
-{
-private:
-	PlayerController m_controller;
-	CameraController m_controller_cam;
-	EnemyBehaviour m_enemy_behaviour;
-	
+class Game : public spe::IScript
+{	
 public:
-	s2d::EngineConfig config;
+	
+	// Gets called on start of the engine
+	void Start() override;
 
-	void start() override;
-	void update() override;
+	// Gets called once per frame
+	void Update() override;
 };
 
