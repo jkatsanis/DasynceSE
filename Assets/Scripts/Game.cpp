606a68e9d68a0c422f3a67dd60b7e66e1a258977
jkatsanis <katsanis18@gmail.com>
2024-01-22 12:04:39 +0100

    anims

--- Assets/Scripts/Game.cpp
+++ Assets/Scripts/Game.cpp
@@ -4,6 +4,9 @@ void Game::Start()
 {
 	this->m_PlayerController.Start(this->EngineConfig);
 	this->m_LevelUI.Start(this->EngineConfig);
+
+	spe::Sprite* woa = this->EngineConfig.ptr_Sprites->GetByName("Water_1");
+	woa->Animator.Play("WaterFlow");
 }
 
 void Game::Update()
